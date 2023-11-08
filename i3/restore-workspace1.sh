9ddd253c4fdd96cffa70fe480f557f228467f761
Kibertod <cyber704d@gmail.com>
2023-11-09 01:11:49 +0300

    бля не помню чё

--- i3/restore-workspace1.sh
+++ i3/restore-workspace1.sh
@@ -3,6 +3,6 @@
 i3-msg "workspace 1; append_layout ~/.config/i3/workspace1_layout.json"
 
 kitty --hold --class "kitty-free-terminal" --name "kitty" &
-kitty --hold --class "kitty-clock" --name "kitty" tuime --format "%H:%M:%S" -c white -c blue &
+kitty --hold --class "kitty-clock" --name "kitty" tclock -c white &
 kitty --hold --class "kitty-ncspot" --name "kitty" ncspot &
 kitty --hold --class "kitty-cava" --name "kitty" cava &
