80b16e3ced72d75f53b3c9da6993db9b60082771
Kibertod <cyber704d@gmail.com>
2023-11-29 13:16:36 +0300

    всяко разное

--- eww/scripts/mediaplayer.py
+++ eww/scripts/mediaplayer.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from time import sleep
+
 
 def main():
     match sys.argv[1]:
@@ -12,7 +12,7 @@ def main():
         case "set_time":
             length = float(exec_command("playerctl --player=ncspot metadata mpris:length")) / 1_000_000
             time = float(sys.argv[2])
-            position = length * ( time / 100 )
+            position = length * time / 100
             exec_command(f"playerctl --player=ncspot position {position}")
         case "get_artist":
             artist = exec_command("playerctl --player=ncspot metadata artist")
@@ -24,18 +24,10 @@ def main():
             if len(song) > 33:
                 song = song[:30] + "..."
             print(song)
-        case "waybar":
-            artist = exec_command("playerctl --player=ncspot metadata artist")
-            if len(artist) > 30:
-                artist = artist[:28] + "..."
-            song = exec_command("playerctl --player=ncspot metadata title")
-            if len(song) > 45:
-                song = song[:42] + "..."
+        case "playing":
             playing = exec_command("playerctl --player=ncspot status") == "Playing"
             playing = "" if playing else ""
-            print('{ "text": "' + f"{playing} {artist} - {song}" + '"}')
-        
-    
+            print(playing)
 
 
 def exec_command(command):
@@ -43,6 +35,7 @@ def exec_command(command):
     res = stream.read().rstrip("\n")
     stream.close()
     return res
+
 
 if __name__ == "__main__":
     main()
